1bc054ab13419ef898415bba78513c0f931ad471
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-13 11:01:12 -0300

    feat: implementando o componente GameOption

--- src/components/ GameOption/index.js
+++ src/components/ GameOption/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styles from './styles.module.scss';
 
 function GameOption() {
   return (
-    <div>
-      
+    <div >
+        <button>
+          <i class="bi bi-patch-plus"></i>
+          <span>Adição</span>
+        </button>
+        <button>
+          <i class="bi bi-dash-circle-dotted"></i>
+          <span>Subtração</span>
+        </button>
+        <button>
+          <i class="bi bi-x-octagon"></i>
+          <span>Multiplicação</span>
+        </button>
+        <button>
+          <i class="bi bi-slash-square"></i>
+          <span>Divisão</span>
+        </button>
     </div>
   );
 }
