fd685d1e1010f1c73b318312297e35aa87f926e9
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-13 11:25:47 -0300

    refactor: removendo app.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 import Home from './pages/Home';
+import '../src/styles/global.scss'
 
 function App() {
   return (
