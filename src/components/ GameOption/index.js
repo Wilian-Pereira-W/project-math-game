2ded2bca1e6e21a26d8831a8040652e5edbce4d4
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-13 13:06:49 -0300

    feat: adicionando o useNavigate para navegar para page de jogo

--- src/components/ GameOption/index.js
+++ src/components/ GameOption/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 function GameOption() {
+  const navigation = useNavigate();
   return (
     <div className={styles.container}>
-        <button>
+        <button
+          value="Sum"
+          onClick={({target}) => navigation(`/jogo/${target.value}`)}
+        >
           <i class="bi bi-patch-plus"></i>
           <span>Adição</span>
         </button>
-        <button>
+        <button
+          value="Subtraction"
+          onClick={({target}) => navigation(`/jogo/${target.value}`)}
+        >
           <i class="bi bi-dash-circle-dotted"></i>
           <span>Subtração</span>
         </button>
-        <button>
+        <button
+          value="Multiplication"
+          onClick={({target}) => navigation(`/jogo/${target.value}`)}
+        >
           <i class="bi bi-x-octagon"></i>
           <span>Multiplicação</span>
         </button>
-        <button>
+        <button
+          value="Division"
+          onClick={({target}) => navigation(`/jogo/${target.value}`)}
+        >
           <i class="bi bi-slash-square"></i>
           <span>Divisão</span>
         </button>
