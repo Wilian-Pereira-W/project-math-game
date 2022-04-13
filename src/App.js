42b3db91782dfe6abd6bf4afaf6646f5c70f0991
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-13 13:07:13 -0300

    feat: importando a page de game

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import Home from './pages/Home';
 import '../src/styles/global.scss'
+import Game from './pages/Game';
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/jogo/:slug" element={<Game />}/>
     </Routes>
    </BrowserRouter>
   );
