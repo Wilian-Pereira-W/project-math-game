8b2a297d185583e8cddabbe5da02ddef9779970d
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-13 23:54:05 -0300

    feat: atualizando o path da rota

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/jogo/:slug" element={<Game />}/>
+      <Route path="/:slug" element={<Game />}/>
     </Routes>
    </BrowserRouter>
   );
