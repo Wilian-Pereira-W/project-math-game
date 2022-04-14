c3e6a0469e1c6f5dbe6e64c609bfd472315565f5
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-13 23:48:12 -0300

    feat: adicionando as rotas no button

--- src/components/ GameOption/index.js
+++ src/components/ GameOption/index.js
@@ -1,39 +1,46 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import { useNavigate } from 'react-router-dom';
 
 function GameOption() {
-  const navigation = useNavigate();
+
   return (
     <div className={styles.container}>
-        <button
-          value="Sum"
-          onClick={({target}) => navigation(`/jogo/${target.value}`)}
-        >
-          <i class="bi bi-patch-plus"></i>
-          <span>Adição</span>
-        </button>
-        <button
-          value="Subtraction"
-          onClick={({target}) => navigation(`/jogo/${target.value}`)}
-        >
-          <i class="bi bi-dash-circle-dotted"></i>
-          <span>Subtração</span>
-        </button>
-        <button
-          value="Multiplication"
-          onClick={({target}) => navigation(`/jogo/${target.value}`)}
-        >
-          <i class="bi bi-x-octagon"></i>
-          <span>Multiplicação</span>
-        </button>
-        <button
-          value="Division"
-          onClick={({target}) => navigation(`/jogo/${target.value}`)}
-        >
-          <i class="bi bi-slash-square"></i>
-          <span>Divisão</span>
-        </button>
+        <a href='/sum'>
+          <button
+            type="button"
+            value="sum"
+          >
+            <i className="bi bi-patch-plus"></i>
+            <span value="a">Adição</span>
+          </button>
+        </a>
+        <a href='/subtraction'>
+          <button
+            type="button"
+            value="Subtraction"
+          >
+            <i value="Subtraction" className="bi bi-dash-circle-dotted"></i>
+            <span value="Subtraction">Subtração</span>
+          </button>
+        </a>
+        <a href='/multiplication'>
+          <button
+            type="button"
+            value="Multiplication"
+          >
+            <i className="bi bi-x-octagon"></i>
+            <span>Multiplicação</span>
+          </button>
+        </a>
+        <a href='/division'>
+          <button
+            type="button"
+            value="Division"
+          >
+            <i className="bi bi-slash-square"></i>
+            <span>Divisão</span>
+          </button>
+        </a>
     </div>
   );
 }
